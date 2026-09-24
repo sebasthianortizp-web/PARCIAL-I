0c839ce73ce359401a33d798b09068bb1039ff17
sebasthianortizp-web <sebasthian.ortizp@uqvirtual.edu.co>
2026-09-24 15:58:38 -0500

    Define Habitacion class with attributes and relationships
    Added attributes and relationships to Habitacion class.

--- hotel/src/main/java/uniquindio/edu/co/model/Habitacion.java
+++ hotel/src/main/java/uniquindio/edu/co/model/Habitacion.java
@@ -1,4 +1,14 @@
 package uniquindio.edu.co.model;
 
 public class Habitacion {
+  //Atributos de la clase Habitaciones
+    public int numero;
+    public int piso;
+    public String tipo;
+    public String capacidad;
+    public double precioNoche;
+    public String estado;
+    //Relaciones de la clase Habitacion
+    private List<Huesped> listHabitaciionHuespedes;
+    private List<Servicio> listHabitaciionServicios;
 }
